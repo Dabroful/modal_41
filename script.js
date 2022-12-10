5dd2680fc38c5811a560de823ee4f7795c925ee3
Vitaly Kovalev <kovalevvitalii@mail.ru>
2022-12-10 09:27:23 +0300

    5-3
    5-3

--- script.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const modalTitle = document.querySelector('.modal_title');
-const modalSubmit = document.querySelector('.modal_submit');
-const modalLabel = document.querySelectorAll('.modal_label');
-const modalFieldset = document.querySelector('.modal_fieldset');
-const modalCheckbox = document.querySelector('.modal_checkbox');
-const modalInput = document.querySelector('.modal_input');
-const modalTotal = document.querySelector('.modal_total');
